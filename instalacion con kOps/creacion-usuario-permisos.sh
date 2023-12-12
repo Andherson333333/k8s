6898bbeb5eb664192fa3cb5fc2ad31e9b761855e
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2023-12-11 22:52:29 -0400

    Update creacion-usuario-permisos.sh

--- instalacion con kOps/creacion-usuario-permisos.sh
+++ instalacion con kOps/creacion-usuario-permisos.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-echo "Enter the IAM group and user name:"
+echo "Ingrese el nombre del grupo IAM:"
+read groupname
+
+echo "Ingrese el nombre del usuario IAM:"
 read username
 
 # Crear el grupo IAM
-aws iam create-group --group-name $username
+aws iam create-group --group-name $groupname
 
 # Adjuntar políticas al grupo IAM
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --group-name $username
-aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --group-name $username
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonRoute53FullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/IAMFullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonVPCFullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess --group-name $groupname
+aws iam attach-group-policy --policy-arn arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess --group-name $groupname
 
 # Crear el usuario IAM
 aws iam create-user --user-name $username
 
 # Agregar el usuario al grupo IAM
-aws iam add-user-to-group --user-name $username --group-name $username
+aws iam add-user-to-group --user-name $username --group-name $groupname
 
 # Crear una clave de acceso para el usuario IAM
 aws iam create-access-key --user-name $username
