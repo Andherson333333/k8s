826a1e61a38b1c635446a63523aebb332543595b
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2023-12-17 23:14:53 -0400

    Update install-binary.sh

--- instalacion k0s multi-worker/install-binary.sh
+++ instalacion k0s multi-worker/install-binary.sh
@@ -14,3 +14,11 @@ mv k0sctl-linux-x64 k0sctl
 
 #Mover de ruta
 sudo mv k0sctl /usr/local/bin/
+
+# Instalacion de kubeclt
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt update
+sudo apt-get install -y  kubectl
+
