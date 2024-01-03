b0a1929ca07e871a704656f8e27fed3cd5cb2859
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-01-03 05:34:14 -0400

    Delete ingress+metallb/metallb-instal.sh

--- ingress+metallb/metallb-instal.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-Metallb_Version=$(curl -s https://api.github.com/repos/metallb/metallb/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//')
-echo "MetalLB Version: $Metallb_Version"
-
-wget https://raw.githubusercontent.com/metallb/metallb/v$Metallb_Version/config/manifests/metallb-native.yaml
-
-
