44ac5da7efd48219935014e032605aecb4e29ef5
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-01-03 05:27:15 -0400

    Update instal-metallb-nginx.sh

--- ingress+metallb/instal-metallb-nginx.sh
+++ ingress+metallb/instal-metallb-nginx.sh
@@ -1,1 +1,9 @@
+#!/bin/sh
+
+Metallb_Version=$(curl -s https://api.github.com/repos/metallb/metallb/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//')
+echo "MetalLB Version: $Metallb_Version"
+
+wget https://raw.githubusercontent.com/metallb/metallb/v$Metallb_Version/config/manifests/metallb-native.yaml
+
+
 
