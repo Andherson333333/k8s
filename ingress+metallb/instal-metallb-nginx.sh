ad6546dcf48c41cb57f1a421e6a0da6acea6c86a
Andherson R <85946986+Andherson333333@users.noreply.github.com>
2024-01-03 05:28:30 -0400

    Update instal-metallb-nginx.sh

--- ingress+metallb/instal-metallb-nginx.sh
+++ ingress+metallb/instal-metallb-nginx.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+##Metallb
 Metallb_Version=$(curl -s https://api.github.com/repos/metallb/metallb/releases/latest | grep tag_name | cut -d '"' -f 4 | sed 's/v//')
 echo "MetalLB Version: $Metallb_Version"
-
 wget https://raw.githubusercontent.com/metallb/metallb/v$Metallb_Version/config/manifests/metallb-native.yaml
+
+##Nginx-ingress
+url="https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/baremetal/deploy.yaml"
+# Obtén la versión desde el enlace
+Version=$(curl -s $url | grep 'image: quay.io/kubernetes-ingress-controller/nginx-ingress-controller' | awk -F ':' '{print $3}' | cut -d '"' -f 1)
+# Imprime la versión obtenida
+echo "Versión del Ingress Nginx Controller: $Version"
+# Descarga el manifiesto YAML usando wget
+wget -O ingress-nginx-manifest.yaml $url
 
 
 
